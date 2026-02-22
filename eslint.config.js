eb03a82799aa479f43920632ae108230c1a7ed70
Rohit Goswami <rgoswami@ieee.org>
2026-02-22 06:49:54 +0100

    feat: initial project scaffolding
    Build system (bun + vite + vitest), lint config, pixi for docs env.

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    typescript: true,
+    ignores: ['.github', 'dist', 'node_modules', '*.yml', '*.yaml', '*.toml'],
+  },
+  {
+    rules: {
+      'style/brace-style': ['error', '1tbs'],
+      'style/arrow-parens': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'antfu/consistent-list-newline': 'off',
+      'ts/no-unused-expressions': 'off',
+      'jsonc/no-useless-escape': 'off',
+    },
+  },
+  {
+    files: ['package.json'],
+    rules: {
+      'style/eol-last': 'off',
+    },
+  },
+)
